32bcf509d201dc24b41c6f739990f8195c1f7914
Sean Melvin <seanmelvin8595@gmail.com>
2019-10-17 09:27:57 -0400

    basic bones of the game

---
+++ javascript/game.js
@@ -0,0 +1,14 @@
+function makeid(length) {
+    var result           = '';
+    var characters       = 'abcdefghijklmnopqrstuvwxyz';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+       result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+ }
+ 
+ console.log(makeid(5));
+
+ //found on stack overflow, selects random letter for user to guess
+ 
